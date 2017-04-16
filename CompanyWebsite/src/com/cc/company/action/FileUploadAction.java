658d82ed0df5a229383bd1500fb69c35c07904e6
cckevincyh <835287880@qq.com>
2017-04-16 22:18:08 +0800

    解决文件上传的重名问题

--- CompanyWebsite/src/com/cc/company/action/FileUploadAction.java
+++ CompanyWebsite/src/com/cc/company/action/FileUploadAction.java
@@ -37,14 +37,14 @@ public class FileUploadAction {
 	public void fileUpload() throws IOException{
         //设置上传的路径
         String path = ServletActionContext.getServletContext().getRealPath("/ProductImg");
-        
+        uploadFileName = +System.currentTimeMillis()+"_"+uploadFileName;
         File file = new File(path, uploadFileName);
         try {
 
             FileUtils.copyFile(upload, file);
             upload.delete();
             
-            //将，要传入页面的数据变成json格式
+            //将要传入页面的数据变成json格式
             JSONObject jsonObject=new JSONObject();
             String path2 = "ProductImg/"+uploadFileName;
             jsonObject.put("path", path2);
