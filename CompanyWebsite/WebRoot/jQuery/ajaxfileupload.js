efae8b56fa3b4677ea2566d5e2cf23f3d9ef4c74
cckevincyh <835287880@qq.com>
2017-04-17 10:14:54 +0800

    解决ajaxFileUpload.js报错问题

--- CompanyWebsite/WebRoot/jQuery/ajaxfileupload.js
+++ CompanyWebsite/WebRoot/jQuery/ajaxfileupload.js
@@ -1,6 +1,16 @@
 // JavaScript Document
 jQuery.extend({
+	handleError: function( s, xhr, status, e )      {
+        // If a local callback was specified, fire it
+        if ( s.error ) {
+            s.error.call( s.context || s, xhr, status, e );
+        }
 
+        // Fire the global callback
+        if ( s.global ) {
+            (s.context ? jQuery(s.context) : jQuery.event).trigger( "ajaxError", [xhr, s, e] );
+        }
+    },
     createUploadIframe: function(id, uri)
  {
    //create frame
